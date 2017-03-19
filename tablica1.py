85c4c2b0c056eb759b2c7e8d5990b8ce2d9bfbb7
Stefan <stefan5andonov@yahoo.com>
2017-03-20 00:30:49 +0100

    Second

---
+++ tablica1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+if __name__ == "__main__":
+    m = input()
+    n = input()
+    x = input()
+    # vasiot kod pisuvajte go tuka
+    tablica = {}
+    print (sorted(tablica.items()))
